bae5385aeb95e838549a3e6f8a55df7ba220bd64
melloyuljacky <mellouljacky@hotmail.com>
2018-08-07 09:29:34 +0200

    aaa

---
+++ projetVoiture/src/metier/Voiture.java
@@ -0,0 +1,17 @@
+package metier;
+
+public class Voiture {
+	private String marque;
+
+	
+	public Voiture(String marque) {
+		
+		this.marque = marque;
+	}
+
+
+	public String toString() {
+		return "Voiture [marque=" + marque + "]";
+	}
+
+}
---
+++ projetVoiture/src/user/Test.java
@@ -0,0 +1,11 @@
+package user;
+import metier.Voiture;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(new Voiture("renault"));
+
+	}
+
+}
